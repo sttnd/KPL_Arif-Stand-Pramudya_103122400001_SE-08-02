55a94a6201d185cbfa5af5a84d0ddc45e69fad86
rvest.p <standpramudya.arif@gmail.com>
2026-03-10 21:18:35 +0700

    feat: add modul 3

---
+++ 03_GUI_dengan_HTML_dan_CSS/index.js
@@ -0,0 +1,7 @@
+const editorElement = document.getElementById("editor-kecil");
+const charCountElement = document.getElementById("hf");
+
+editorElement.addEventListener("input", (event) => {
+    const textLength = event.target.value.length;
+    charCountElement.textContent = textLength;
+});
