442672d1030d9d61f19e5f46f6f39c40dba4765a
rvest.p <standpramudya.arif@gmail.com>
2026-06-10 00:22:04 +0700

    feat: add tm modul 14

---
+++ 14_Clean_Code/TM/tm14.js
@@ -0,0 +1,21 @@
+// Kode A
+function processUser(user) {
+  if (user) {
+    if (user.isActive) {
+      if (user.hasPermission) {
+        return doSomething(user)
+      }
+    }
+  }
+  return null
+}
+
+// Kode B
+function processUser(user) {
+  if (!isValidCandidate(user)) return null;
+  return doSomething(user);
+}
+
+function isValidCandidate(user) {
+  return user && user.isActive && user.hasPermission;
+}
